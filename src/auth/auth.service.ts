b2b59d6a1150328783abf01b5317fe059029cc90
Stanley <41586666+repository@users.noreply.github.com>
2020-05-03 16:22:38 -0700

    Add steam strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { sign } from "jsonwebtoken";
+
+import { Player } from "src/interfaces/steam/GetPlayerSummaries";
+import { Payload } from "src/interfaces/jwt/Payload";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private config: ConfigService) {}
+
+  private readonly JWT_SECRET = this.config.get<string>("JWT_SECRET");
+
+  async validateLogin(player: Player): Promise<string> {
+    try {
+      const payload = { steamid: player.steamid } as Payload;
+      const token = sign(payload, this.JWT_SECRET, { expiresIn: "7d" });
+
+      return token;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        "An error has occured during the sign-in process.",
+        error.message
+      );
+    }
+  }
+}
---
+++ src/auth/steam.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-steam";
+import { ConfigService } from "@nestjs/config";
+import { Player } from "src/interfaces/steam/GetPlayerSummaries";
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class SteamStrategy extends PassportStrategy(Strategy, "steam") {
+  constructor(config: ConfigService, private authService: AuthService) {
+    super({
+      returnURL: config.get<string>("API_URL") + "/auth/callback",
+      realm: config.get<string>("API_URL"),
+      apiKey: config.get<string>("STEAM_KEY")
+    });
+  }
+
+  async validate(_identifier: string, profile: any, done: Function): Promise<void> {
+    try {
+      const player: Player = profile._json;
+      const token = await this.authService.validateLogin(player);
+
+      return done(null, token);
+    } catch (error) {
+      return done(error, null);
+    }
+  }
+}
