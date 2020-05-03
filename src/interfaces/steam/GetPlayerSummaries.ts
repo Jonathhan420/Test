da66ca2fe31a16ebecbb3cb99b8a621f3a2fcf68
Stanley <41586666+repository@users.noreply.github.com>
2020-05-03 15:58:33 -0700

    Add player summary interface

---
+++ src/interfaces/steam/GetPlayerSummaries.ts
@@ -0,0 +1,26 @@
+export interface PlayerSummaries {
+  response: Response;
+}
+
+export interface Response {
+  players: Player[];
+}
+
+export interface Player {
+  steamid: string;
+  communityvisibilitystate: number;
+  profilestate: number;
+  personaname: string;
+  profileurl: string;
+  avatar: string;
+  avatarmedium: string;
+  avatarfull: string;
+  personastate: number;
+  realname: string;
+  primaryclanid: string;
+  timecreated: number;
+  personastateflags: number;
+  loccountrycode: string;
+  locstatecode: string;
+  loccityid: number;
+}
