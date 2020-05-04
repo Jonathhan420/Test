046daaa79341f33ae4282ad850365bb529b42afc
Stanley <41586666+repository@users.noreply.github.com>
2020-05-04 15:02:58 -0700

    Add method to upsert user from a Player object

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+import { User } from "src/entities/user.entity";
+import { Player } from "src/interfaces/steam/GetPlayerSummaries";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
+  private readonly AVATAR_ID = /[0-9a-f]{40}/;
+
+  private async getBySteamId(steamid: string) {
+    return this.userRepo.findOneOrFail({ steamid });
+  }
+
+  async upsertUserFromPlayer(player: Player) {
+    let user: User;
+    try {
+      user = await this.getBySteamId(player.steamid);
+    } catch {
+      user = new User({
+        steamid: player.steamid,
+        name: player.personaname,
+        avatar: player.avatar.match(this.AVATAR_ID)[0],
+        private: player.communityvisibilitystate !== 3
+      });
+    }
+
+    return this.userRepo.save(user);
+  }
+}
