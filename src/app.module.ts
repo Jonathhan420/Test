832fc3630e1a3a0b22307014ec7fa387f114a21e
Stanley <41586666+repository@users.noreply.github.com>
2020-05-03 22:32:22 -0700

    Add TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: "mysql",
+        host: config.get<string>("DB_HOST"),
+        port: config.get<number>("DB_PORT"),
+        username: config.get<string>("DB_USER"),
+        password: config.get<string>("DB_PASS"),
+        database: "development",
+        synchronize: true,
+
+        ...(config.get<string>("ENV_NODE") == "production" && {
+          synchronize: false,
+          database: "production"
+        })
+      })
     }),
     AuthModule
   ],
