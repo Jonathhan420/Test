473547295c3cfb8f0abff10622df406196132e85
Stanley <41586666+repository@users.noreply.github.com>
2020-05-05 21:27:53 -0700

    Add method to get a player from steamid

--- src/steam/steam.module.ts
+++ src/steam/steam.module.ts
@@ -1,9 +1,21 @@
-import { Module } from "@nestjs/common";
+import { Module, HttpModule } from "@nestjs/common";
 
 import { SteamService } from "./steam.service";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        baseURL: "http://api.steampowered.com/",
+        params: {
+          key: config.get<string>("STEAM_KEY")
+        }
+      })
+    })
+  ],
+  exports: [SteamService],
   providers: [SteamService]
 })
 export class SteamModule {}
--- src/steam/steam.service.ts
+++ src/steam/steam.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, HttpService } from "@nestjs/common";
+import { AxiosResponse } from "axios";
+
+import { PlayerSummaries } from "src/interfaces/steam/GetPlayerSummaries";
 
 @Injectable()
-export class SteamService {}
+export class SteamService {
+  constructor(private axios: HttpService) {}
+
+  async getPlayer(steamid: string) {
+    const response: AxiosResponse<PlayerSummaries> = await this.axios
+      .get("/ISteamUser/GetPlayerSummaries/v0002/", {
+        params: { steamids: steamid }
+      })
+      .toPromise();
+
+    return response.data.response.players[0];
+  }
+}
