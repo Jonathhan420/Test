fe0a557d4254f910be131f9baabae8583f404ab4
Stanley <41586666+repository@users.noreply.github.com>
2020-05-08 19:57:25 -0700

    Removed unused dependency

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -2,10 +2,10 @@ import { Module } from "@nestjs/common";
 
 import { StatsService } from "./stats.service";
 import { SteamModule } from "src/steam/steam.module";
-import { UserModule } from "src/user/user.module";
 
 @Module({
-  imports: [SteamModule, UserModule],
+  imports: [SteamModule],
+  exports: [StatsService],
   providers: [StatsService]
 })
 export class StatsModule {}
