b3a0f0e8d47598cebc77f0da58c81b80686066dc
Stanley <41586666+repository@users.noreply.github.com>
2020-05-08 20:26:59 -0700

    Create a new user if they dont exist

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "src/entities/user.entity";
 import { UserController } from "./user.controller";
 import { SteamModule } from "src/steam/steam.module";
+import { StatsModule } from "src/stats/stats.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), SteamModule],
+  imports: [TypeOrmModule.forFeature([User]), SteamModule, StatsModule],
   exports: [UserService],
   providers: [UserService],
   controllers: [UserController]
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,18 +5,40 @@ import { Repository } from "typeorm";
 import { User } from "src/entities/user.entity";
 import { Player } from "src/interfaces/steam/GetPlayerSummaries";
 import { SteamService } from "src/steam/steam.service";
+import { StatsService } from "src/stats/stats.service";
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User) private userRepo: Repository<User>,
-    private steamService: SteamService
+    private steamService: SteamService,
+    private statsService: StatsService
   ) {}
 
   private readonly AVATAR_ID = /[0-9a-f]{40}/;
 
+  private async getInternalId(steamid: string) {
+    return (await this.userRepo.findOne({ steamid })).id || null;
+  }
+
   private async getBySteamId(steamid: string) {
     return this.userRepo.findOneOrFail({ steamid });
+  }
+
+  private async createNewUser(player: Player) {
+    const id = await this.getInternalId(player.steamid);
+
+    let user = new User({
+      id,
+      steamid: player.steamid,
+      name: player.personaname,
+      avatar: player.avatar.match(this.AVATAR_ID)[0],
+      private: player.communityvisibilitystate !== 3
+    });
+
+    user = await this.statsService.getStatsForUser(user);
+
+    return user;
   }
 
   async upsertUserFromPlayer(player: Player) {
@@ -24,12 +46,7 @@ export class UserService {
     try {
       user = await this.getBySteamId(player.steamid);
     } catch {
-      user = new User({
-        steamid: player.steamid,
-        name: player.personaname,
-        avatar: player.avatar.match(this.AVATAR_ID)[0],
-        private: player.communityvisibilitystate !== 3
-      });
+      user = await this.createNewUser(player);
     }
 
     return this.userRepo.save(user);
